118547154d95df7c8a8033e9635a59ddf947a377
Cezar <cezar.godinho@fatec.sp.gov.br>
2020-03-08 20:55:01 -0300

    Third commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import Contador from './Contador';
+import PlacarContainer from './PlacarContainer';
+
+const dados = {
+    partida: {
+        estadio: "Maracanã/RJ",
+        data: "01/06/2020",
+        horario: "19h",
+    },
+    casa: {
+        nome: "Vasco",
+    },
+    visitante: {
+        nome: "Flamengo",
+    }
+};
+
 
 export default class App extends React.Component {
-    render(){
+    render() {
         return (
-            <Contador />
+            <PlacarContainer partida={dados.partida}
+                casa={dados.casa}
+                visitante={dados.visitante} />
         );
     }
 }
